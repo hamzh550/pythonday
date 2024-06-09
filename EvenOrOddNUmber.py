5d28ca857984bdb4456777d73e9aef7400bdb519
HAMZH ALZEQRI <149473275+hamzh550@users.noreply.github.com>
2024-06-09 15:52:33 +0100

    Create EvenOrOddNUmber.py

---
+++ EvenOrOddNUmber.py
@@ -0,0 +1,7 @@
+#this program will checkj if user input is even or odd number 
+number=float(input("please enter your number.?\n"))
+
+if number %2==1:
+    print("this is an Odd number")
+else:
+    print("this is an even number")
